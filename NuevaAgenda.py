4d99a3990ec2848c3f35ff87225d13916a63b61d
KennyRoDi <132397563+KennyRoDi@users.noreply.github.com>
2023-06-12 22:24:58 -0600

    Update NuevaAgenda.py
    Cambios en la función que agrega las discusiones.

--- NuevaAgenda.py
+++ NuevaAgenda.py
@@ -1,12 +1,9 @@
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
-
-from Agenda import agenda as Agenda, lista as Lista, persona as Persona, discusion as Discusion, puntos as Puntos, apartados as Apartados
+from agenda import agenda as Agenda, lista as Lista, persona as Persona, discusion as Discusion, puntos as Puntos, apartados as Apartados
 from datetime import datetime
 
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
+from datetime import datetime
 
 class MiAgenda(Agenda):
-
     def __init__(self, titulo: str, fecha: datetime) -> None:
         super().__init__()
         self.titulo: str = titulo
@@ -20,38 +17,40 @@ class MiAgenda(Agenda):
         else:
             self.participantes.agregar(nombre, apellido1, apellido2)
 
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
-
     def agregar_apartado(self, apartado: str):
         if self.apartados is None:
             self.apartados = MiApartado(apartado)
         else:
             self.apartados.agregar(apartado)
 
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
-
-    def agregar_punto(self, punto:str, apartado:str):
+    def agregar_punto(self, punto: str, apartado: str):
         if self.apartados is not None:
             apartado_actual = self.apartados
+
             while apartado_actual is not None:
                 if apartado_actual.apartado == apartado:
                     if apartado_actual.puntos is None:
                         apartado_actual.puntos = MisPuntos(punto)
                     else:
                         nodo_punto = apartado_actual.puntos
+
                         while nodo_punto.sig is not None:
                             nodo_punto = nodo_punto.sig
                         nodo_punto.sig = MisPuntos(punto)
                     break
                 else:
-                    apartado_actual = apartado_actual.sig
+                    if apartado_actual.sig is None:
+                        apartado_actual.sig = MiApartado(apartado)
+                        apartado_actual.sig.puntos = MisPuntos(punto)
+                        break
+                    else:
+                        apartado_actual = apartado_actual.sig
         else:
             self.apartados = MiApartado(apartado)
             self.apartados.puntos = MisPuntos(punto)
 
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
 
-    def agregar_discusión(self, persona, apartado, punto, discusión):
+    def agregar_discusion(self, persona, apartado, punto, discusión):
         discusion_agregar = MisDiscusiones(persona, discusión)
         pos_actual = self.apartados
         while pos_actual is not None:
@@ -68,8 +67,6 @@ class MiAgenda(Agenda):
     @property
     def asDict(self):
         return {"Título":self.titulo,"fecha":self.fecha._str_(),"participantes":self.participantes.asList}
-    
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
 
 def crear_agenda(titulo:str, fecha:datetime):
     """Funcion que se encarga de crear la instancia de la agenda
@@ -80,8 +77,6 @@ def crear_agenda(titulo:str, fecha:datetime):
     """
     global agenda
     agenda = MiAgenda(titulo, fecha)
-
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
 
 def agregar_participante(nombre:str, apellido1:str, apellido2:str):
     """Funcion que se encarga de agregar un participante a la agenda
@@ -94,10 +89,7 @@ def agregar_participante(nombre:str, apellido1:str, apellido2:str):
     global agenda
     agenda.agregar_participante(nombre,apellido1,apellido2)
 
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
-
 class MiLista(Lista):
-
     def __init__(self) -> None:
         super().__init__()
 
@@ -107,11 +99,8 @@ class MiLista(Lista):
         else:
             r.sig = self._agregar(r.sig, e)
             return r
-        
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
 
 class MiPersona(Persona):
-
     def __init__(self, nombre: str, apellido1: str, apellido2: str) -> None:
         super().__init__(nombre, apellido1, apellido2)
 
@@ -125,8 +114,6 @@ class MiPersona(Persona):
         else:
             r.sig = self._agregar(r.sig, p)
             return r
-        
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
 
     @property
     def asList(self):
@@ -144,8 +131,6 @@ class MiPersona(Persona):
 
     def __str__(self) -> str:
         return "{0} {1} {2}".format(self.nombre, self.apellido1, self.apellido2)
-    
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
 
 def participante_asList():
     """Funcion que se encarga de agregar los participantes de la sesion a una lista
@@ -158,23 +143,15 @@ def participante_asList():
         return agenda.participantes.asList
     except:
         return []
-    
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
 
 class MisDiscusiones(Discusion):
 
     def __init__(self, persona: Persona, discusion: str) -> None:
         super().__init__(persona, discusion)
 
-    def agregar_discusion(self, persona, apartado, punto, discusion):
-        discusion_nueva = MisDiscusiones(persona, discusion)
-        self._agregar_discusion_recursivo(self.apartados, apartado, punto, discusion_nueva)
-
-    def agregar_discusion(self, persona: MiPersona, apartado: str, punto: str, discusion: str):
-        if self.discusiones is None:
-            self.discusiones = MisDiscusiones(persona, apartado, punto, discusion)
-        else:
-            self.discusiones.agregar_discusion(persona, apartado, punto, discusion)
+    def agregar_discusion (self, persona, discusion):
+        discusion_agregar = MisDiscusiones(persona, discusion)
+        self._agregar(self, discusion_agregar)
 
     def _agregar(self, r, d):
         if r is None:
@@ -182,8 +159,6 @@ class MisDiscusiones(Discusion):
         else:
             r.sig = self._agregar(r.sig, d)
             return r
-        
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
 
 def crear_discusion(participante:str, apartado:str, punto:str, discusion:str):
     """Funcion que se encarga de crear la discusion
@@ -196,8 +171,6 @@ def crear_discusion(participante:str, apartado:str, punto:str, discusion:str):
     """
     global agenda
     agenda.agregar_discusion(participante, apartado, punto, discusion)
-
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
 
 class MiApartado(Apartados):
     def __init__(self, apartado: str) -> None:
@@ -231,8 +204,6 @@ class MiApartado(Apartados):
             current = current.sig
 
         return result
-    
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
 
     @property
     def asDict(self):
@@ -244,8 +215,6 @@ class MiApartado(Apartados):
             current = current.sig
 
         return result
-    
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
 
 def agregar_apartado(apartado:str):
     """Funcion que se encarga de agregar los apartados a la agenda
@@ -255,8 +224,6 @@ def agregar_apartado(apartado:str):
     """
     global agenda
     agenda.agregar_apartado(apartado)
-
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
 
 class MisPuntos(Puntos):
     def __init__(self, punto: str) -> None:
@@ -274,8 +241,6 @@ class MisPuntos(Puntos):
         else:
             self._agregar(r.sig, e)
 
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
-
     @property
     def asList(self):
         if self==None:
@@ -289,8 +254,6 @@ class MisPuntos(Puntos):
             return [r.punto]
         else:
             return [r.punto]+self._asList(r.sig)
-        
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
 
 def agregar_puntos(apartado:str, punto:str):
     """Se encarga de agregar los puntos a los apartados de la agenda
@@ -302,8 +265,6 @@ def agregar_puntos(apartado:str, punto:str):
     global agenda
     agenda.agregar_punto(punto, apartado)
 
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
-
 def puntos_asDict() -> list:
     """Funcion que se encarga de pasar los puntos a listas
 
@@ -313,8 +274,5 @@ def puntos_asDict() -> list:
     global agenda
     try:
         return agenda.apartados.asDict
-    
     except:
-        return {"": ""}
-    
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#-----------------------------------------------------------------------------------------------------------------------------#
+        return {"" : ""}
